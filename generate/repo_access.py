733920c8b5f36a69bf7c65ef1df91773c38e7606
Dustin J. Mitchell <dustin@mozilla.com>
2019-10-09 17:00:27 +0000

    Bug 1574666 - give all TC projects access to the Azure testing secret

--- generate/repo_access.py
+++ generate/repo_access.py
@@ -12,6 +12,9 @@ from tcadmin.resources import Role
 repo_access = [
     {
         'scopes': [
+            # The account and secret for the Azure testing storage account.
+            # This is secret but ok for use by PRs.
+            'secrets:get:project/taskcluster/testing/azure',
         ],
         'worker-pools': [
             'proj-taskcluster/ci',
