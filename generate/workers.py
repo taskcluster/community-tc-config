88581c294e5613c638f242d4522c5542f61850a2
Miles Crabill <mcrabill@mozilla.com>
2019-11-01 13:18:38 -0700

    add deepspeech win worker-pool

--- generate/workers.py
+++ generate/workers.py
@@ -256,17 +256,34 @@ def standard_aws_generic_worker_win2012r2(**cfg):
     return rv
 
 @worker_pool_type
+def aws_generic_worker_deepspeech_win(**cfg):
+    """
+    Build a deepspeech-win-b worker instance in AWS
+    """
+    images = {
+        'us-east-1': 'ami-06fe09656e2529efc',
+        'us-west-1': 'ami-0fa8f5d5f2441a296',
+        'us-west-2': 'ami-008a206b2da4e0a82',
+    }
+    rv = base_aws_generic_worker_config(
+        imageIds=images,
+        instanceTypes={"m5d.2xlarge": 1},
+        **cfg)
+
+    return rv
+
+@worker_pool_type
 def aws_generic_worker_deepspeech_win_b(**cfg):
     """
     Build a deepspeech-win-b worker instance in AWS
     """
-    deepspeech_win_b_images = {
+    images = {
         'us-east-1': 'ami-0081dc6e528d277f6',
         'us-west-1': 'ami-085a3abd95ca9301d',
         'us-west-2': 'ami-01e9bc39c5157adc1',
     }
     rv = base_aws_generic_worker_config(
-        imageIds=deepspeech_win_b_images,
+        imageIds=images,
         instanceTypes={"m5d.2xlarge": 1},
         **cfg)
 
