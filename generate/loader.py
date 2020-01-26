d27a30092db886b0336d961cec62b79975674fac
Dustin J. Mitchell <dustin@mozilla.com>
2020-01-26 16:29:34 -0500

    split projects into separate files

--- generate/loader.py
+++ generate/loader.py
@@ -5,5 +5,34 @@
 # obtain one at http://mozilla.org/MPL/2.0/.
 
 from tcadmin.util.config import LocalLoader
+import os
 
 loader = LocalLoader()
+
+
+class YamlDirectory(dict):
+    """
+    Similar to tc-admin's ConfigDict, this loads data from all `.yml` files in
+    `cls.directory`, merging the results.
+    """
+
+    @classmethod
+    async def load(cls, loader):
+        res = {}
+        for file in os.listdir(cls.directory):
+            if not file.endswith(".yml"):
+                continue
+            file = os.path.join(cls.directory, file)
+            data = await loader.load(file, parse="yaml")
+            assert isinstance(data, dict), "{} is not a YAML object".format(
+                cls.filename
+            )
+            for k, v in data.items():
+                if k in res:
+                    raise RuntimeError(f"{file}: another file already defined key {k}")
+                res[k] = cls.Item(k, **cls.transform_item(v))
+        return res
+
+    @classmethod
+    def transform_item(cls, item):
+        return item
