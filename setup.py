caedca29b2186324d54887b687d5a2e28d05e1f6
Pete Moore <pmoore@mozilla.com>
2021-06-14 16:51:42 +0200

    Dustin offboarding

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="Configuration for Taskcluster at https://community-tc.services.mozilla.com/",
     author="Dustin Mitchell",
     author_email="dustin@mozilla.com",
-    url="https://github.com/djmitche/community-tc-config",
+    url="https://github.com/mozilla/community-tc-config",
     packages=find_packages("."),
     install_requires=[
         "tc-admin~=3.0.1",
