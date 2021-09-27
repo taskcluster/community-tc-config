587677ef551d609c7d88af92f4b767141a7db3e1
Pete Moore <pmoore@mozilla.com>
2021-09-27 11:31:21 +0200

    Retain owner/group and rwx permissions of docker-worker livelog certificate file

--- imagesets/docker-worker/bootstrap.sh
+++ imagesets/docker-worker/bootstrap.sh
@@ -7,7 +7,9 @@ WORKER_CERT="$(dirname $(realpath $0))/worker-cert.pem"
 
 for CERT_FILE in /etc/star_taskcluster-worker_net.crt /etc/taskcluster/secrets/worker_livelog_tls_cert; do
   if [ -f "${CERT_FILE}" ]; then
-    sudo cp "${WORKER_CERT}" "${CERT_FILE}"
+    # use tee rather than cp so that file owner/group and read/write/execute
+    # permissions are retained
+    cat "${WORKER_CERT}" | sudo tee "${CERT_FILE}"
   fi
 done
 
