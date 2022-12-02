541a4594dda765ef4e5a81ee47894ced19a9c685
Matt Boris <mboris@mozilla.com>
2022-12-02 11:57:33 -0500

    Update to v45.0.1

--- imagesets/generic-worker-ubuntu-22-04-staging/bootstrap.sh
+++ imagesets/generic-worker-ubuntu-22-04-staging/bootstrap.sh
@@ -4,7 +4,7 @@ set -exv
 exec &> /var/log/bootstrap.log
 
 # Version numbers ####################
-TASKCLUSTER_REF='v45.0.0'
+TASKCLUSTER_REF='v45.0.1'
 ######################################
 
 function retry {
--- imagesets/generic-worker-ubuntu-22-04/bootstrap.sh
+++ imagesets/generic-worker-ubuntu-22-04/bootstrap.sh
@@ -4,7 +4,7 @@ set -exv
 exec &> /var/log/bootstrap.log
 
 # Version numbers ####################
-TASKCLUSTER_VERSION='v45.0.0'
+TASKCLUSTER_VERSION='v45.0.1'
 ######################################
 
 function retry {
