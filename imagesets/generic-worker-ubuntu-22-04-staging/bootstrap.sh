df28ab6f26be57ec0e6a8af29a075e41759f4603
Matt Boris <mboris@mozilla.com>
2023-07-19 12:03:35 -0400

    Update staging commit

--- imagesets/generic-worker-ubuntu-22-04-staging/bootstrap.sh
+++ imagesets/generic-worker-ubuntu-22-04-staging/bootstrap.sh
@@ -6,7 +6,7 @@ exec &> /var/log/bootstrap.log
 ##############################################################################
 # TASKCLUSTER_REF can be a git commit SHA, a git branch name, or a git tag name
 # (i.e. for a taskcluster version number, prefix with 'v' to make it a git tag)
-TASKCLUSTER_REF='e0080f3f2f887cb154b3f2d494bc6df8ca7d5455'
+TASKCLUSTER_REF='b672d66fadb06789723a4816b7f6294a05a412de'
 ##############################################################################
 
 function retry {
