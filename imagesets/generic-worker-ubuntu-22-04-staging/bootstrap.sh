01487ee638a64ff3e7460087d9354e6ba2b0d07c
Matt Boris <mboris@mozilla.com>
2024-03-22 14:40:22 -0400

    Test PR #6925

--- imagesets/generic-worker-ubuntu-22-04-staging/bootstrap.sh
+++ imagesets/generic-worker-ubuntu-22-04-staging/bootstrap.sh
@@ -6,7 +6,7 @@ exec &> /var/log/bootstrap.log
 ##############################################################################
 # TASKCLUSTER_REF can be a git commit SHA, a git branch name, or a git tag name
 # (i.e. for a taskcluster version number, prefix with 'v' to make it a git tag)
-TASKCLUSTER_REF='816c2e8144ca87ab54cda4e32268f36cf0d62d86'
+TASKCLUSTER_REF='c50705ea6e3c821f59d2e712345c87578141ef23'
 ##############################################################################
 
 function retry {
