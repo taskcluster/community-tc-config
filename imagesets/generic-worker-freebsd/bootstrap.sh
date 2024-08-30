73a00451f6b35af2846f16d08b28e85cf670799c
Matt Boris <mboris@mozilla.com>
2024-08-30 14:44:45 -0400

    chore: bump tc to v68.0.3

--- imagesets/generic-worker-freebsd/bootstrap.sh
+++ imagesets/generic-worker-freebsd/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/csh
 
 # Version numbers ####################
-setenv TASKCLUSTER_VERSION v68.0.1
+setenv TASKCLUSTER_VERSION v68.0.3
 ######################################
 
 pkg update
--- imagesets/generic-worker-win2022-staging/bootstrap.ps1
+++ imagesets/generic-worker-win2022-staging/bootstrap.ps1
@@ -1,4 +1,4 @@
-$TASKCLUSTER_VERSION = "v68.0.1"
+$TASKCLUSTER_VERSION = "v68.0.3"
 
 # use TLS 1.2 (see bug 1443595)
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
