3f42f40b031a1245af323e91be668ae695bc7e47
Matt Boris <mboris@mozilla.com>
2024-09-26 10:24:06 -0400

    chore: bump tc to v71.0.0

--- imagesets/generic-worker-ubuntu-24-04/bootstrap.sh
+++ imagesets/generic-worker-ubuntu-24-04/bootstrap.sh
@@ -4,7 +4,7 @@ set -exv
 exec &> /var/log/bootstrap.log
 
 # Version numbers ####################
-TASKCLUSTER_VERSION='v70.0.0'
+TASKCLUSTER_VERSION='v71.0.0'
 ######################################
 
 function retry {
