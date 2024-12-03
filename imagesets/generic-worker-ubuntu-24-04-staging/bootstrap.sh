9375d726ec99dc6f9ecb4bf94bf7e06da68affe3
Matt Boris <mboris@mozilla.com>
2024-12-03 13:10:10 -0500

    fix: install `linux-modules-extra-gcp` for `snd-aloop`

--- imagesets/generic-worker-ubuntu-24-04-staging/bootstrap.sh
+++ imagesets/generic-worker-ubuntu-24-04-staging/bootstrap.sh
@@ -168,10 +168,9 @@ sed '/platform-vkms/d' /lib/udev/rules.d/61-mutter.rules > /etc/udev/rules.d/61-
 # https://help.ubuntu.com/community/KVM/Installation
 retry apt-get install -y qemu-kvm bridge-utils
 
+# install extra modules for snd-aloop kernel module
 if [ '%MY_CLOUD%' == 'google' ]; then
   retry apt-get install -y linux-modules-extra-gcp
-elif [ '%MY_CLOUD%' == 'aws' ]; then
-  retry apt-get install -y linux-modules-extra-aws
 fi
 echo 'options snd-aloop enable=1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 index=0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31' > /etc/modprobe.d/snd-aloop.conf
 echo 'snd-aloop' >> /etc/modules
