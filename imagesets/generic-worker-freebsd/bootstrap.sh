539290a242780332918027bb3a6865c34d020489
Matt Boris <mboris@mozilla.com>
2024-12-04 13:38:31 -0500

    chore: bump TC to v75.0.1

--- imagesets/generic-worker-freebsd/bootstrap.sh
+++ imagesets/generic-worker-freebsd/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/csh
 
 # Version numbers ####################
-setenv TASKCLUSTER_VERSION v74.0.0
+setenv TASKCLUSTER_VERSION v75.0.1
 ######################################
 
 pkg update
--- imagesets/generic-worker-win2022/bootstrap.ps1
+++ imagesets/generic-worker-win2022/bootstrap.ps1
@@ -1,4 +1,4 @@
-$TASKCLUSTER_VERSION = "v74.0.0"
+$TASKCLUSTER_VERSION = "v75.0.1"
 
 # Write-Log function for logging with RFC3339 format timestamps
 function Write-Log {
