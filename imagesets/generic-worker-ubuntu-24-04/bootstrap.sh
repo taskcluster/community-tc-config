cdbcd6f7f382d4514c86cb93e58671bfcd4eb106
Matt Boris <mboris@mozilla.com>
2025-04-08 15:16:04 -0400

    chore: bump to TC v83.5.1

--- imagesets/generic-worker-ubuntu-24-04/bootstrap.sh
+++ imagesets/generic-worker-ubuntu-24-04/bootstrap.sh
@@ -4,7 +4,7 @@ set -exv
 exec &> /var/log/bootstrap.log
 
 # Version numbers ####################
-TASKCLUSTER_VERSION='v83.5.0'
+TASKCLUSTER_VERSION='v83.5.1'
 ######################################
 
 function retry {
