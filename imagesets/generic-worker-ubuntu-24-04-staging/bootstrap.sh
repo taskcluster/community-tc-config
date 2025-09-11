ffb12932184d74ab3c71faa41fe69068d803682c
Matt Boris <mboris@mozilla.com>
2025-09-11 12:14:22 -0400

    Revert "chore: revert ubuntu staging back to main"
    This reverts commit bf56800d770ef78e458dd095d639ea460959ff45.

--- imagesets/generic-worker-ubuntu-24-04-staging/bootstrap.sh
+++ imagesets/generic-worker-ubuntu-24-04-staging/bootstrap.sh
@@ -6,7 +6,7 @@ exec &> /var/log/bootstrap.log
 ##############################################################################
 # TASKCLUSTER_REF can be a git commit SHA, a git branch name, or a git tag name
 # (i.e. for a taskcluster version number, prefix with 'v' to make it a git tag)
-TASKCLUSTER_REF='main'
+TASKCLUSTER_REF='matt-boris/d2gDontPassEnv'
 TASKCLUSTER_REPO='https://github.com/taskcluster/taskcluster'
 ##############################################################################
 
