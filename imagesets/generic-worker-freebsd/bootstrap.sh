bd07a18b393cb12303779969b8f461b726031638
Pete Moore <pmoore@mozilla.com>
2025-10-20 23:15:36 +0200

    chore: bump to TC v91.0.2

--- imagesets/generic-worker-freebsd/bootstrap.sh
+++ imagesets/generic-worker-freebsd/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/csh
 
 # Version numbers ####################
-setenv TASKCLUSTER_VERSION v91.0.1
+setenv TASKCLUSTER_VERSION v91.0.2
 ######################################
 
 pkg update
--- imagesets/generic-worker-ubuntu-24-04/bootstrap.sh
+++ imagesets/generic-worker-ubuntu-24-04/bootstrap.sh
@@ -4,7 +4,7 @@ set -exv
 exec &> /var/log/bootstrap.log
 
 # Version numbers ####################
-TASKCLUSTER_VERSION='v91.0.1'
+TASKCLUSTER_VERSION='v91.0.2'
 ######################################
 
 function retry {
