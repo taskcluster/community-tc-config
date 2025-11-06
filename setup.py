48bdd17bf8fba10885730003a15806c5ef22ef4a
Matt Boris <mboris@mozilla.com>
2025-11-06 10:00:19 -0500

    require tc-admin 5.1.1

--- setup.py
+++ setup.py
@@ -9,16 +9,17 @@ setup(
     url="https://github.com/taskcluster/community-tc-config",
     packages=find_packages("."),
     install_requires=[
-        "tc-admin>=5.1.0",
+        "tc-admin>=5.1.1",
         "json-e>=4.7.1",
         "ruamel.yaml",
     ],
     setup_requires=["pytest-runner"],
     tests_require=["pytest-mock", "pytest-asyncio"],
     classifiers=[
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
     ],
 )
